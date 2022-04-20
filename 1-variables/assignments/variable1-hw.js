acb44fa25c090942e12b0753eae0f106b9299141
Odalis Novoa Reyes <odalisnovoa@Odaliss-MacBook-Air.local>
2022-04-20 17:33:25 -0400

    variable 1 homework

---
+++ 1-variables/assignments/variable1-hw.js
@@ -0,0 +1,46 @@
+// Create a .js file that satisfies assignment below 
+// Copy the final assignment text into this assignment to submit
+// For each exercise you'll want to log the result to the console to check your work
+
+//1. Create a set of variables that define an animal (age, height, name, etc...). Must be at least 6 variables.
+const breed = 'Husky'
+console.log(breed)
+
+const fname = 'Toby'
+console.log(fname)
+
+const age = '1_year_old'
+console.log(age)
+
+const furColor = 'blank_and_white'
+console.log(furColor)
+
+const eyeColor = 'blue'
+console.log(eyeColor)
+
+const walkStyle = 'on_all_fours'
+console.log(walkStyle)
+
+
+
+
+
+//2. Create a set of variables that define a vehicle. Must be at least 6 variables.
+const makeYear = '2010'
+console.log(makeYear)
+
+const carColor = 'red'
+console.log(carColor)
+
+const modelType = 'SUV'
+console.log(modelType)
+
+const wheels = '4_wheels'
+console.log(wheels)
+
+const driveStyle = 'manual'
+console.log(driveStyle)
+
+const Cname = 'Cadillac'
+console.log(Cname)
+
