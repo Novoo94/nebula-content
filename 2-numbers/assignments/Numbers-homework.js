79d1882883495b3aaa061de69ff9345ab7259137
Odalis Novoa Reyes <odalisnovoa@Odaliss-MacBook-Air.local>
2022-04-21 18:20:27 -0400

    numbers homework

---
+++ 2-numbers/assignments/Numbers-homework.js
@@ -0,0 +1,63 @@
+ // Remeber to console.log() to debug your code and check your solutions!
+
+// Declare a variable and assign it to a numeric value
+// Half that value and re-assign it to the previous variable 
+let rj = 8
+rj /= 2
+
+console.log(rj)
+
+// Create two negative numbers, assign them to variables and subtract one from the other together 
+const de = -3
+const dop = -7
+  
+console.log(de - dop)
+
+// Create three numbers, assign them to variables and multiply them together 
+const sop = 3
+const pos = 4
+const set = 2
+
+console.log(sop * pos * set)
+
+// Create two even numbers, assign them to variables and divide one from the other 
+let a = 24
+let b = 12
+
+console.log(a/b)
+
+// Create two odd numbers, assign them to variables and find the remainder of dividing one by the other 
+let c = 3
+let d = 17
+
+console.log(d%c)
+
+// find the minimum value in a set of numbers: 10,20,30,40,50 
+console.log(Math.min(10,20,30,40,50))
+
+// find the maximum value in a set of numbers: -1,-2,-3,-4,-5 
+console.log(Math.max(-1,-2,-3,-4,-5 ))
+
+// Create a variable and find it's remainder, if divided by 2
+
+const tea = 2247
+console.log(tea%2)
+
+// find the square root of 144 
+console.log(Math.sqrt(144))
+
+// find the value of 3 to the power of 7 
+console.log(Math.pow(3,7))
+
+// find the value of 73.2 rounded 
+
+console.log(Math.round(73.2))
+
+// find the rounded-down value of 9.9 
+console.log(Math.floor(9.9))
+
+// find the absolute value of -1231
+console.log(Math.abs(-1231))
+
+
+
