0c0fe5e2f4f96657334c753228acddfcc96ba78c
Odalis Novoa Reyes <odalisnovoa@Odaliss-MacBook-Air.local>
2022-04-20 20:22:36 -0400

    numbers practice 2

---
+++ 2-numbers/practice/1number-practice.js
@@ -0,0 +1,94 @@
+//////PRACTICE//////
+
+//Add together two numbers.
+console.log(2+2)
+
+//Subtract two numbers.
+console.log(20-15)
+
+//Multiply two numbers.
+console.log(4*5)
+
+//Divide two numbers.
+console.log(10/5)
+
+//Find the remainder of 223 divided by 3.
+
+console.log(223%3)
+
+//Store the value in a variable produced by dividing 300 by 100. Declare that value with the let keyword.
+
+let x = 300/100
+console.log(x)
+
+//Increment that variable by 1 using the increment operator.
+
+let a = 3;
+let b = ++a;
+console.log('b',b)
+
+
+//console.log that variable, or type it directly in the console to view it's value.
+console.log(b)
+
+//Use a multiply assignment operator to change the value of the variable to the product of multiplying it by 5.
+
+let y = 5
+y *=5
+
+//console.log that variable, or type it directly in the console to view it's value
+
+console.log(y)
+
+
+
+
+// Declare a variable and assign it to a numeric value
+//let x = "7"
+
+// Double that value and re-assign it to the previous variable
+x= x*2
+//or x* = 2
+
+
+// Check it worked by logging to the console
+console.log(x)
+
+// Create two numbers, assign them to variables and add them together
+//const a = 5
+//const b = 20
+
+// Check it worked by logging to the console \
+console.log(a + b)
+
+// Create three numbers, assign them to variables and subtract the smaller two from the largest
+const o = 7 
+const t = 8 
+const d = 25
+let e = 25 - ( 8 + 7)
+
+// Check it worked by logging to the console
+console.log(d - t + o)
+
+// Create two even numbers, assign them to variables and divide one from the other
+const j = 24 
+const v = 12 
+
+// // Check it worked by logging to the console
+console.log(j/v)
+
+// Create two odd numbers, assign them to variables and multiply them together
+const bo = 13 
+const po = 3
+const pop = bo * po
+
+// Check it worked by logging to the console
+console.log(pop)
+
+// Create a variable and log it's remainder, if divided by 3, to the console 
+const pr = 25
+console.log(pr % 3)
+
+// Answer in plain text, what is modulo especially useful for?
+// //helps figure out if a number is even or odd such as 3%2 (which has a remainder) 
+//
---
+++ 2-numbers/practice/2number-practice.js
@@ -0,0 +1,55 @@
+
+// For these questions use the Math methods that JavaScript includes
+// All Math methods: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+// find the square root of 25
+// log it to the console
+console.log(Math.sqrt(25))
+
+// find the value of 3 to the power of 5
+// log it to the console
+console.log(Math.pow(3,5))
+
+
+// find the value of 7.4 rounded
+// log it to the console
+
+console.log(Math.round(7.4))
+
+// find the rounded-up value of 7.4
+// log it to the console
+
+console.log(Math.ceil(7.4))
+
+
+// find the absoloute value of -13
+// log it to the console
+
+let six = Math.abs(-13)
+console.log(six)
+
+
+// find the minimum value in a set of numbers: 12,13,14,15,2,-1
+// log it to the console
+console.log(Math.min(12,13,14,15,2,-1))
+
+
+
+// find the maximum value in a set of numbers: 12,13,14,15,2,-1
+// log it to the console
+console.log(Math.max(12,13,14,15,2,-1))
+
+
+// create a random number 
+const number = Math.random()
+
+// log it to the console
+console.log(number)
+
+
+// Create a random number between 1-10 
+const Number = Math.random() * 10
+
+// Number between 1 -10
+
+console.log(Number)
