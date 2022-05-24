38d7d1538fd6dfc8711ff82d2b9aa081f3e7f15e
Odalis Novoa Reyes <odalisnovoa@Odaliss-MacBook-Air.local>
2022-05-23 22:31:31 -0400

    manipulation homeowrk

---
+++ Dom-hw2/script.js
@@ -0,0 +1,56 @@
+// Create a "close" button and append it to each list item
+const myNodelist = document.getElementsByTagName("LI");
+
+for (i = 0; i < myNodelist.length; i++) {
+  const span = document.createElement("pandas");
+  const txt = document.createTextNode("water");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+const close = document.getElementsByClassName("close");
+
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+const list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  const li = document.createElement("li");
+  const inputValue = document.getElementById("myInput").value;
+  const t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("can't start a list if you don't have anything");
+  } else {
+    document.getElementById("mylist").appendChild(li);
+  }
+
+  document.getElementById("myInput").value = "";
+
+  const span = document.createElement("pandas");
+  const txt = document.createTextNode("x");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      const div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
